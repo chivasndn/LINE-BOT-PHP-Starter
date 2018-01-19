cd5df7fd37795a82099c5375724ea324cd6ac274
chivasndn <35572783+chivasndn@users.noreply.github.com>
2018-01-19 15:28:29 +0700

    Add files via upload

--- bot.php
+++ bot.php
@@ -48,51 +48,51 @@ if (!is_null($events['events'])) {
 }
 echo "OK";
 function get_value(){
-	
-	$return_txt = "";
-	
-	date_default_timezone_set("Asia/Bangkok");
-	
-	$crypto_currency_list = array("XRP","EVX")
-	
-	$count = count($crypto_currency_list);
-	
-	for($k=0;$k<$count;$k++){
-	
-	$max = 1;
-	$i = 0;
-	$pairing_id = 0;
-	
-	$rate =0;
-	
-	$json = file_get_contents('https://bx.in.th/api/');
-	$obj = json_decode($json);
-	
-	foreach($obj as $val){
-		// print_r($val);
 		
-		if($val->secondary_currency==$crypto_currency_list[$k]&&$val->primary_currency=="THB"){
-			$pairing_id = $val->pairing_id;
-			//echo "\n------------------------------------\n";
-			break;
+		$return_txt = "";
+		
+		date_default_timezone_set("Asia/Bangkok");
+		
+		$crypto_currency_list = array("XRP","EVX");
+		
+		$count = count($crypto_currency_list);
+		
+		for($k=0;$k<$count;$k++){
+			
+			$max = 1;
+			$i = 0;
+			$pairing_id = 0;
+			
+			$rate = 0;
+			
+			$json = file_get_contents('https://bx.in.th/api/');
+			$obj = json_decode($json);
+			
+			foreach($obj as $val){
+				// print_r($val);
+				
+				if($val->secondary_currency==$crypto_currency_list[$k]&&$val->primary_currency=="THB"){
+					$pairing_id = $val->pairing_id;
+					//echo "\n------------------------------------\n";
+					break;
+				}
+				
+			}
+			
+			while($i<$max){
+				$json = file_get_contents('https://bx.in.th/api/trade/?pairing='.$pairing_id);
+				$obj = json_decode($json);
+				
+				if(number_format($obj->trades[0]->rate, 2)!=0){
+					$rate = number_format($obj->trades[0]->rate, 2);
+					$return_txt = $return_txt . $crypto_currency_list[$k]." : ".$rate."\n";
+					//echo date("H:i:s")."\t".$rate."\n";
+					//sleep(3);
+					$i++;
+				}
+			}
+			
 		}
 		
-	}
-	
-	while($i<$max){
-		$json = file_get_contents('https://bx.in.th/api/trade/?pairing='.$pairing_id);
-		$obj = json_decode($json);
-		
-		if(number_format($obj->trades[0]->rate, 2)!=0){
-			$rate = number_format($obj->trades[0]->rate, 2);
-			$return_txt = $return_txt . $crypto_currency_list[$k]." : ".$rate."\n";
-			//echo date("H:i:s")."\t".$rate."\n";
-			//sleep(3);
-			$i++;
-		}
-	}
-	
-}
-	
-	return $return_txt;
-}
+		return $return_txt;
+	}		
